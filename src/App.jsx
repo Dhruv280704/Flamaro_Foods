66f0094bb9ae119e2beb7d5719237f01bdcaf41a
Dhruv280704 <Dhruvking425@gmail.com>
2025-04-27 17:02:12 +0530

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Success from "./Pages/Success";
-import Error from "./pages/Error";
+import Error from "./Pages/Error";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
